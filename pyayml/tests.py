c3e4d54d0253b732b2b2e6321aee70756d97fb9f
doggy <aleksandr.rakov@gmail.com>
2013-12-15 15:30:44 +0400

    cats,curr,show,adnd test

--- pyayml/tests.py
+++ pyayml/tests.py
@@ -1,9 +1,31 @@
 # -*- coding: utf8 -*-
 import unittest
+import pyayml
 
 class ViewTests(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_my_view(self):
-        self.assertEqual(info['project'], 'pyayml')
+    def test_001(self):
+        obj=pyayml.YaYml()
+        data={
+            'shop':{
+                'name':'shop name',
+                'company': 'my cool company',
+                'url' : 'http://www.mail.ru',
+            },
+            'currencies':[
+                {'id':'RUR','rate':'CBFR'},
+            ],
+            'categories': [
+                {'id':'1','name':u'АЛЛ Продуктс'},
+                {'id':'2','name':'sub cat products','parentId':'1'},
+            ]
+        }
+        obj.set_shop(data['shop'])
+        obj.set_currencies(data['currencies'])
+        obj.set_categories(data['categories'])
+
+        print obj.get_xml()
+
+        #self.assertEqual(info['project'], 'pyayml')
